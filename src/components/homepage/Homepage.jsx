4d8014eea82622a2e8031c6877c0b71138ef0050
Sahillrathore <sahil921031@gmail.com>
2024-09-23 23:36:29 +0530

    mins

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 
 const images = [
-  { img: 'https://cdn.sanity.io/images/tdrhge4k/production/d6c83f64afe535d4e980564808ba5569f1e8bedd-1201x631.png'},
-  { img: 'https://imagedelivery.net/OOahao1ejQQJFOoUSSCKqw/game/mines-1/8zSgB95U.png/w=384,h=384,fit=cover,quality=80,format=auto,blur=0'}
+  { link: 'dice', img: 'https://cdn.sanity.io/images/tdrhge4k/production/d6c83f64afe535d4e980564808ba5569f1e8bedd-1201x631.png'},
+  {link: 'mines', img: 'https://imagedelivery.net/OOahao1ejQQJFOoUSSCKqw/game/mines-1/8zSgB95U.png/w=384,h=384,fit=cover,quality=80,format=auto,blur=0'}
 ]
 
 const Homepage = () => {
@@ -27,13 +27,21 @@ const Homepage = () => {
     fetchData();
   },[])
   return (
-    <>
-        <h1 className='text-3xl font-bold text-zinc-100'>Homepage</h1>
+    <div className='p-4'>
+        <h1 className='text-3xl font-bold text-zinc-100'>Popular Games</h1>
         
-        <div className="games-cards">
-            
+        <div className="games-cards flex gap-4 my-6">
+            {
+              images.map((img)=>(
+                <Link to='dice' key={img}>
+                  <div className="game-banner h-52 w-48">
+                    <img src={img.img} alt="..img" className='h-full w-full object-cover object-center' />
+                  </div>
+                </Link>
+              ))
+            }
         </div>
-    </>
+    </div>
   )
 }
 
