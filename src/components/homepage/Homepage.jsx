be23bc185a12c08992278d414ed263e64b29de31
Sahillrathore <sahil921031@gmail.com>
2024-09-23 23:33:48 +0530

    cardMap

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -3,6 +3,11 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
+const images = [
+  { img: 'https://cdn.sanity.io/images/tdrhge4k/production/d6c83f64afe535d4e980564808ba5569f1e8bedd-1201x631.png'},
+  { img: 'https://imagedelivery.net/OOahao1ejQQJFOoUSSCKqw/game/mines-1/8zSgB95U.png/w=384,h=384,fit=cover,quality=80,format=auto,blur=0'}
+]
+
 const Homepage = () => {
 
   const [state, setState] = useState([]);
@@ -26,11 +31,7 @@ const Homepage = () => {
         <h1 className='text-3xl font-bold text-zinc-100'>Homepage</h1>
         
         <div className="games-cards">
-            <Link to='dice'>
-             <div className="game-banner h-52 w-48">
-              <img src="https://cdn.sanity.io/images/tdrhge4k/production/d6c83f64afe535d4e980564808ba5569f1e8bedd-1201x631.png" alt="..img" className='h-full w-full object-cover object-center' />
-             </div>
-            </Link>
+            
         </div>
     </>
   )
